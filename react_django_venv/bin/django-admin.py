dca38ce652f793acdb5b6c0c16420e2513a444c4
Chad-Mowbray <mowbray.chad@gmail.com>
2019-07-01 15:06:34 -0500

    first commit

---
+++ react_django_venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/chadm/Desktop/Projects/react_django/react_django_venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
